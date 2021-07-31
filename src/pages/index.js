69f37505aba53409c324b3dffe76a3b81835750f
Jereme <michael@jeremehancock.com>
2021-07-31 18:46:54 -0500

    Updated to CleanBrowsing

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,8 +14,7 @@ import mateogomez from '../images/mateogomez.jpg';
 const Index = () => (
   <Layout>
     <section id="top" className="pt-5 lg:pt-20 lg:mx-10">
-      <div
-        className="container mx-auto px-8 lg:flex mb-10 rounded-lg border border-solid border-none md:border-gray-200 p-5 lg:shadow-2xl m-5">
+      <div className="container mx-auto px-8 lg:flex mb-10 rounded-lg border border-solid border-none md:border-gray-200 p-5 lg:shadow-2xl m-5">
         <div className="text-left lg:w-1/2 lg:pr-10 mb-10">
           <h1 className="text-center lg:text-left text-3xl lg:text-2xl xl:text-3xl font-bold leading-tight italic mb-12">
             "freely ye have received, freely give"
@@ -51,19 +50,17 @@ const Index = () => (
       reverseOrder
       primarySlot={
         <div className="lg:pl-32 xl:pl-48">
-          <h3 className="text-2xl md:text-3xl font-semibold leading-tight">Web Content Filtering</h3>
+          <h3 className="text-2xl md:text-3xl font-semibold leading-tight">
+            Web Content Filtering
+          </h3>
           <p className="mt-8 text-xl font-light leading-relaxed">
-            UbuntuCE is pre-configured with OpenDNS FamilyShield. OpenDNS is an industry leading DNS
+            UbuntuCE is pre-configured with CleanBrowsing. CleanBrowsing is an industry leading DNS
             provider that offers fast, secure, DNS with state of the art content filtering.
           </p>
         </div>
       }
       secondarySlot={
-        <StaticImage
-          src="../images/opendns-family-shield.png"
-          alt="OpenDNS FamilyShield"
-          placeholder="blurred"
-        />
+        <StaticImage src="../images/cleanbrowsing.png" alt="CleanBrowsing" placeholder="blurred" />
       }
     />
     <SplitSection
@@ -100,7 +97,9 @@ const Index = () => (
     <SplitSection
       primarySlot={
         <div className="lg:pr-32 xl:pr-48">
-          <h3 className="text-2xl md:text-3xl font-semibold leading-tight">Software for Churches</h3>
+          <h3 className="text-2xl md:text-3xl font-semibold leading-tight">
+            Software for Churches
+          </h3>
           <p className="mt-8 text-xl font-light leading-relaxed">
             UbuntuCE is a great choice for Churches and includes OpenLP, as well as Presenter by WP.
           </p>
@@ -138,11 +137,7 @@ const Index = () => (
             <TeamCard image={jeremehancock} name="Jereme Hancock" title="Creator / Developer" />
           </div>
           <div className="flex-1 px-3">
-            <TeamCard
-              image={russmacshane}
-              name="Russ MacShane"
-              title="QA / Developer"
-            />
+            <TeamCard image={russmacshane} name="Russ MacShane" title="QA / Developer" />
           </div>
           <div className="flex-1 px-3">
             <TeamCard image={mateogomez} name="Mateo Gomez" title="Torrent Manager" />
@@ -154,13 +149,11 @@ const Index = () => (
       <hr className="container" />
     </div>
     <LabelText className="mt-10 mb-3 md:mb-5 text-gray-600 text-center">FAQ</LabelText>
-    <section id="faq" className="lg:mx-10">
-      <div
-        className="container mx-auto rounded-lg border border-solid border-none md:border-gray-200 p-5 lg:shadow-2xl"
-      >
+    <section id="faq" className="lg:mx-10 md:mb-32">
+      <div className="container mx-auto rounded-lg border border-solid border-none md:border-gray-200 p-5 lg:shadow-2xl">
         <div>
           <div className="mb-3 font-bold text-xl italic">
-            Is Ubuntu Christian Edition officially supported by Canonical?
+            Is UbuntuCE officially supported by Canonical?
           </div>
           <div className="mb-3">
             <div className="pl-3">
@@ -171,42 +164,38 @@ const Index = () => (
             </div>
           </div>
           <div className="mb-3 font-bold text-xl italic">
-            Is Ubuntu Christian Edition a new distribution of Ubuntu or a derivative?
+            Is UbuntuCE a new distribution of Ubuntu or a derivative?
           </div>
           <div className="mb-3">
             <div className="pl-3">
-              Well it is really neither. Ubuntu Christian Edition is based directly from the
-              standard Ubuntu distribution and contains extra Christian software as well as a few
-              additional tools to make the transition to Ubuntu easier for a Linux newcomer. The
-              graphical changes are minor and are only intended to tailor the project to Christians.
+              Well it is really neither. UbuntuCE is based directly from the standard Ubuntu
+              distribution and contains extra Christian software and is pre-configured with web
+              content filtering. The graphical changes are minor and are only intended to tailor the
+              project to Christians.
             </div>
           </div>
           <div className="mb-3 font-bold text-xl italic">
-            Why should I use the Ubuntu Christian Edition if I can install the same software to the
-            default Ubuntu?
+            Why should I use the Ubuntu CE if I can install the same software to the default Ubuntu?
           </div>
           <div className="mb-3">
             <div className="pl-3">
-              The concept behind the Ubuntu Christian Edition is not to deviate from the Ubuntu
-              community. It is intended to draw a larger Christian base to the already thriving
-              community of Ubuntu users. The Ubuntu Christian Edition simply makes it easier for
-              Christians who are new to Linux to see the power of Ubuntu combined with the added
-              benefit of having the best available Linux Christian software pre-installed.
+              The concept behind the UbuntuCE is not to deviate from the Ubuntu community. It is
+              intended to draw a larger Christian base to the already thriving community of Ubuntu
+              users. UbuntuCE simply makes it easier for Christians who are new to Linux to see the
+              power of Ubuntu combined with the added benefit of having the best available Linux
+              Christian software pre-installed.
             </div>
           </div>
         </div>
       </div>
     </section>
-    <div className="flex justify-center">
-      <hr className="container" />
-    </div>
     <section className="mx-10" id="download">
-      <div
-        className="container mx-auto my-20 rounded-lg border border-solid border-gray-200 text-center p-10 shadow-2xl"
-      >
+      <div className="container mx-auto mt-10 md:mt-20 mb-20 rounded-lg border border-solid border-gray-200 text-center p-10 shadow-2xl">
         <h3 className="text-xl md:text-5xl font-semibold">Ready to try UbuntuCE?</h3>
         <p className="mt-8">
-          <Href to="https://sourceforge.net/projects/ubuntuce/files/latest/download"><Button>Download Now</Button></Href>
+          <Href to="https://sourceforge.net/projects/ubuntuce/files/latest/download">
+            <Button>Download Now</Button>
+          </Href>
         </p>
         <p className="mt-10">
           Need{' '}
@@ -214,6 +203,13 @@ const Index = () => (
             help on installing
           </Href>
           ?
+        </p>
+        <p className="mt-5">
+          Already running Ubuntu?{' '}
+          <Href to="https://github.com/mhancoc7/repo.ubuntuce.com#readme">
+            Check out the UbuntuCE Repo
+          </Href>
+          .
         </p>
       </div>
     </section>
