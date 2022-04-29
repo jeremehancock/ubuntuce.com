be4f00c14cfff49a6b5253e9407c280f7283cb76
Jereme <michael@jeremehancock.com>
2022-04-29 14:14:30 -0500

    fixed margins

--- src/components/SplitSection.js
+++ src/components/SplitSection.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const SplitSection = ({ id, primarySlot, secondarySlot, reverseOrder }) => (
-  <section id={id} className="py-5 md:py-16">
+  <section id={id} className="py-5 md:py-16 md:mx-10">
     <div className="container mx-auto px-8 items-center flex flex-col lg:flex-row rounded-lg md:border md:border-solid md:border-gray-200 p-5 md:shadow-2xl">
       <div className="lg:w-1/2">{primarySlot}</div>
       <div
