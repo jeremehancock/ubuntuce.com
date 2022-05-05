b481fb0354adb4e53e1800cedc25db815da314bb
Jereme <michael@jeremehancock.com>
2022-05-04 21:36:56 -0500

    clean up

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,4 @@
 import React from 'react';
-import Layout from '../components/layout/Layout';
+import Index from '../pages/index';
 
-const NotFound = () => (
-  <Layout>
-    <h1>Not Found</h1>
-    <p>You just hit a page that doesn&#39;t exist...</p>
-  </Layout>
-);
-
-export default NotFound;
+export default Index;
